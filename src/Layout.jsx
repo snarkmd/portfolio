3d24f9f4c06f95970a6327956df0e3c309752420
bentoumi anes <97307936+snarkmd@users.noreply.github.com>
2025-06-14 02:40:07 +0100

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -100,7 +100,7 @@ const MainLayout = ({ children }) => {
     };
  
     const tl = gsap.timeline({delay:0.5})
-    //tl.add(cubeAnim())
+    tl.add(cubeAnim())
     tl.add(scriptAnim())
     tl.add(layoutAnim())
   },[])
